e3a9a16beecdd36b7083bc003d568391d6d9594e
Mariajulialetro <mariajulialetro@outlook.com>
2021-07-09 09:52:17 -0300

    Csharp

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double R, pi, volume;
+
+            R = double.Parse(Console.ReadLine());
+
+            pi = 3.14159;
+
+            volume = (double) (4 / 3.0) * pi * Math.Pow(R, 3);
+
+            Console.WriteLine("VOLUME = " + volume.ToString("F3", CultureInfo.InvariantCulture));
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
